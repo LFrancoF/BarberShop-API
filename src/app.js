cbfb9b5358a6138cc5004b86ccb0c7342281e798
LFrancoF <gustavo.15.franco.fl@gmail.com>
2023-07-13 06:34:40 -0400

    url front deployed added

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 createAdminAndRoles();
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://barbershop-classic.up.railway.app'],
     credentials: true
 }));
 app.use(morgan('dev'));
