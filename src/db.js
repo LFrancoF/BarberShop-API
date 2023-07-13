61214e58a677a17382800eb322d00a2c48b512e6
LFrancoF <gustavo.15.franco.fl@gmail.com>
2023-07-13 01:44:59 -0400

    rest api barbershop updated v1

--- src/db.js
+++ src/db.js
@@ -1,10 +1,14 @@
 import {createPool} from 'mysql2/promise'
+import {config} from 'dotenv'
 
+config()
+
+console.log(process.env.DB_PASSWORD)
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    port: 3306,
-    database: 'classic_barbershop'
+    host: process.env.DB_HOST || 'localhost', 
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_DATABASE || 'classic_barbershop'
 })
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from './app.js'
-import {pool} from './db.js'
+import {config} from 'dotenv'
 
+config()
 
 app.listen(process.env.PORT || 3030, () => {
     console.log('Server listening at PORT:', process.env.PORT || 3030);
