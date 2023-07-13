d5b0f254d2d6aefb6a55a97a49d205d9deeea86b
LFrancoF <gustavo.15.franco.fl@gmail.com>
2023-07-13 08:30:28 -0400

    origin true

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 createAdminAndRoles();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://barbershop-classic.up.railway.app'],
+    origin: true,
     credentials: true
 }));
 app.use(morgan('dev'));
