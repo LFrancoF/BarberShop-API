99950f4e69b177688b77f307d1deecce14e6615c
LFrancoF <gustavo.15.franco.fl@gmail.com>
2023-07-13 12:25:17 -0400

    samesite secure

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -102,7 +102,7 @@ export const login = async (req, res) => {
 
         //create token for user
         const token = await createAccesToken({id : userFound.idUsuario})
-        res.cookie('token', token, {httpOnly: false})
+        res.cookie('token', token, {httpOnly: true, sameSite: none, secure: true, maxAge: 24 * 60 * 60 * 1000})
 
         //Get idBarber or idClient
         const userWithRol = new Usuario({
